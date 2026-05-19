5b390561d293235d4e314a1cd32976ac96e2c424
Aleystr <alexandresilvadcon@gmail.com>
2026-05-19 16:07:30 -0300

    Feito o desafio 01

--- desafios/desafio-01/explicacao.md
+++ desafios/desafio-01/explicacao.md
@@ -1,13 +1,13 @@
 # Explicação — Desafio 01
 
-**Aluno:** _(seu nome)_  
-**Data:** _(data)_
+**Aluno:** _(Alexandre Silva da Conceição)_  
+**Data:** _(19-05-2026)_
 
 ---
 
 ## O que meu programa faz
 
-_(Descreva em suas palavras o que cada parte do código faz. Não copie o enunciado — explique como você pensou na solução.)_
+_(Calcula a idade do usuário com base na entrada da data de aniversário (dd-mm-yy) do usuário levando em consideração a data de hoje(datetime.now()).)_
 
 ---
 
@@ -15,10 +15,12 @@ _(Descreva em suas palavras o que cada parte do código faz. Não copie o enunci
 
 > Por que é necessário converter o resultado do `input()` antes de calcular a idade? O que acontece se não converter?
 
-_(Sua resposta aqui — use suas próprias palavras. Pode incluir exemplos de código se ajudar a explicar.)_
+_(É necessario pois caso fosse calcular a idade do usuário apenas com base no ano de nascimento, seria precisso fazer uma operação matemática, o que só é possível se o dado cadastrado no input for int ou float. Há a possibilide de não converter o input, como no caso do meu código, no entanto ainda foi preciso converter String em outro momento. )_
 
 ---
 
 ## Dificuldades encontradas
 
 _(Opcional: o que foi difícil? O que você pesquisou para resolver?)_
+
+_(Foi dificil montar a lógica para um sistema que )_
--- desafios/desafio-01/solucao.py
+++ desafios/desafio-01/solucao.py
@@ -1,5 +1,22 @@
 # Desafio 01 — Seu Primeiro Script
-# Aluno: (seu nome aqui)
-# Data:  (data de entrega)
+# Aluno: (Alexandre Silva da Conceição)
+# Data:  (19-05-2026)
 
 # ── Escreva sua solução abaixo ──────────────────────────────────────────────
+
+import re # --- Import do "re" para o uso da funcão re.split() para separar a data de nascimento do usuário em dia, mês e ano. ---
+from datetime import datetime # --- Import datetime para usar data atualizada
+dt_nascimento_usuario = input ("Informe sua data de nascimento(dd-mm-YYYY): ")# --- Pede a Idade do Usuário e armazena a data de nascimento em uma variável. ---
+dt_formatada = re.split(r"[-./_ ]", dt_nascimento_usuario)# --- Separa a data de nascimento do usuário em dia, mês e ano usando a função re.split() e armazena cada parte em uma variável. ---
+dt_now = datetime.now() # -- Armazena na variável a data atual
+dt_split = re.split(r"[-./_ ]", (dt_now.strftime("%d-%m-%Y"))) # --- Separa a data atual em dia, mês e ano usando a função re.split() e armazena cada parte em uma variável. ---
+dia = int(dt_split[0])       - int(dt_formatada[0]) # --- Faz  a conversão da data para dias para poder ser convertido em anos -------- 
+
+if (int(dt_formatada[1]) < int(dt_split[1])): # --- Verifica se o mês de nascimento é menor que o mês atual para calcular a idade corretamente. ---
+    mes = 30 *(int(dt_split[1]) - int(dt_formatada[1])) 
+else:
+    mes = 30 * (int(dt_formatada[1]) - int(dt_split[1]))
+
+ano = 365*(int(dt_split[2])  - int(dt_formatada[2]))
+idade = int((dia + mes + ano)/365) # --- Calcula a idade do usuário em anos e exibe o resultado. ---
+print("A idade do usuario é de:", idade, "anos")
