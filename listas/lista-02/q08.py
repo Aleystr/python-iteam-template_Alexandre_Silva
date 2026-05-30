3e3b8f09bf010390146dcae2cd9dd15d79e4b876
Aleystr <alexandresilvadcon@gmail.com>
2026-05-29 21:05:52 -0300

    Feito a lista 02

--- listas/lista-02/q08.py
+++ listas/lista-02/q08.py
@@ -12,3 +12,43 @@
 # calcular_bonus() sem você verificar o tipo do objeto?
 
 # ── Sua solução abaixo ──────────────────────────────────────────────────────
+class Funcionario ():
+    def __init__ (self, nome, salario):
+        self.nome = nome
+        self.salario = salario
+
+    def calcular_bonus(self):
+        return self.salario * 10
+
+class Gerente(Funcionario):
+    def __init__ (self, nome, salario, departamento):
+        super().__init__(nome,salario)
+        self.departamento = departamento
+
+    def calcular_bonus(self):
+        return super().calcular_bonus()
+
+class Estagiario(Funcionario):
+    def __init__ (self, nome, salario, curso):
+        super().__init__(nome,salario)
+        self.curso = curso
+
+    def calcular_bonus(self):
+        return super().calcular_bonus()
+    
+funcionarios = [ Funcionario("Alice Souza", 4000),
+                 Gerente("Carlo Lima", 9000, "Tecnologia"),
+                 Estagiario("Beatriz Reis", 1500, "Engenharia de Software")]
+
+
+print("----- Relatorio de bonus ------")
+for f in funcionarios:
+    bonus = f.calcular_bonus()
+    print(f"Nome: {f.nome} | Bonus: R$ {bonus:.2f}")  
+        
+"""
+ Em python ao referenciar a propria classe o interpretador
+ faz a busca diretamente na classe real daquele objeto 
+ especifico, se a classefilha implementou o metodo, ela 
+ sobrescreve (@override)
+""" 
--- listas/lista-02/q09.py
+++ listas/lista-02/q09.py
@@ -12,3 +12,60 @@
 # Em q09_resposta.txt: explique a diferença entre _atributo e __atributo em Python.
 
 # ── Sua solução abaixo ──────────────────────────────────────────────────────
+class produto():
+    def __init__ (self, nome: str, preco: float, estoque: int):
+        self.nome = nome
+        self.preco = preco
+        self.__estoque = 0
+        self.repor(estoque)
+
+    @property
+    def preco(self) -> float:
+        return self.__preco
+    
+    @preco.setter
+    def  preco(self, novo_preco: float):
+        if novo_preco <= 0:
+            raise ValueError("O preço do produto deve ser maior que zero.")
+        self.__preco = novo_preco
+
+    @property 
+    def estoque(self) -> int:
+        return self.__estoque
+    
+    def repor(self, quantidade: int):
+        if quantidade < 0:
+            raise ValueError("A quantidade de reposição não pode ser negativada.")
+        self.__estoque += quantidade
+
+    def vender(self, quantidade: int):
+        if quantidade <= 0:
+            raise ValueError("A quantidade de venda deve ser maior que zero")
+        if quantidade > self.__estoque:
+            raise ValueError(f"Estoque insuficiente. Tentativa de vender {quantidade}, mas ha apenas {self.__estoque} em estoque.")
+        self.__estoque -= quantidade
+
+    def __str(self) -> str:
+        return f"produto: {self.nome} | preco: R$ {self.preco:.2f} | estoque:  {self.estoque} unid."
+
+    def __repr__ (self) -> str:
+        return f"Produto(nome = {self.nome!r}, preco = {self.preco}, estoque = {self.estoque})"
+
+if __name__ == "__main__":
+    p = produto("Notebook", 3500, 10)
+    print(p)
+    print(repr(p))
+
+    p.vender(3)
+    print(f"Venda de tres unidades realizadas com sucesso.")
+    print(f"Estoque atual: {p.estoque}")
+    
+    p.repor(5)
+    print(f"reposicao de 5 unidades realizada.")
+    print(f"Estoque atual: {p.estoque}")
+
+    try:
+        print(f"Tentando vender 15 unidades (Estoque disponivel: {p.estoque} )...")
+        p.vender(15)
+    except ValueError as e:
+        print(f"Erro Capturado: {e}")
